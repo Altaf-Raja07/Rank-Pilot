61325b235e8294a8c45e64ede87824f22a514fb0
Altaf Raja <altafraja01076@gmail.com>
2026-08-01 00:37:28 +0530

    feat(auth): implement user authentication context with login, registration, and logout functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
@@ -11,8 +11,11 @@ import RankTracker from "./pages/RankTracker";
 import RankDetail from "./pages/RankDetail";
 import { Toaster } from "react-hot-toast";
 import PixelSplashCursor from "./components/ui/pixel/animations/pixel-splash-cursor";
+import { useApp } from "./context/AppContext";
 
 export default function App() {
+    const {user, loading} = useApp()
+
     const location = useLocation();
 
     const hideNavbar = ["/login", "/register"].includes(location.pathname);
@@ -24,8 +27,8 @@ export default function App() {
             {!hideNavbar && <Navbar />}
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login state="login" />} />
-                <Route path="/register" element={<Login state="register" />} />
+                <Route path="/login" element={user ? <Navigate to="/dashboard" replace /> : <Login state="login" />} />
+                <Route path="/register" element={user ? <Navigate to="/dashboard" replace /> : <Login state="register" />} />
                 <Route element={<ProtectedRoute />}>
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/analyze" element={<Analyze />} />
